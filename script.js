acc0425c89e4f02d8dc3dcf4a452f41d1e07de29
Glen <garpirungu@gmail.com>
2023-09-19 09:41:11 +0300

    README & LICENSE

--- script.js
+++ script.js
@@ -7,13 +7,25 @@ document.addEventListener("DOMContentLoaded", function () {
         hiddenContent.classList.toggle("hidden");
     });
 });
-// JavaScript for mouseover effect
-const hoverButton = document.getElementById("hover-button");
 
-hoverButton.addEventListener("mouseover", function () {
-    hoverButton.style.backgroundColor = "#ff9900"; // Change to a different color on hover
+
+// Get the buttons by their IDs
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+
+// Add event listeners for mouseover and mouseout
+button1.addEventListener('mouseover', () => {
+    button1.style.backgroundColor = 'red';
 });
 
-hoverButton.addEventListener("mouseout", function () {
-    hoverButton.style.backgroundColor = ""; // Reset the color when the mouse moves away
+button1.addEventListener('mouseout', () => {
+    button1.style.backgroundColor = '#333';
+});
+
+button2.addEventListener('mouseover', () => {
+    button2.style.backgroundColor = 'blue';
+});
+
+button2.addEventListener('mouseout', () => {
+    button2.style.backgroundColor = '#333';
 });
